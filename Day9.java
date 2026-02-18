7a00c160cc96a8a5d2a9b79f2e1c022f814e7f28
Harshvardhan <harshvardhan584624@gmail.com>
2026-02-18 15:30:51 +0530

    commit 11

---
+++ Day9.java
@@ -0,0 +1,34 @@
+
+
+// public class Day9 {
+
+//     public static void main (String[] args){
+//         System.out.println("The program is Starting from here.");
+//         try{
+//             int a=10;
+//             int b=0;
+//             int c=a/b;
+//             System.out.println("The result is:"+c);
+//         }catch(ArithmeticException ae){
+//             System.out.println("Exception caught: "+ae);
+//         }finally{
+//             System.out.println("This is always executed");
+//         }
+//         System.out.println("The program is ended");
+//     } 
+// }
+
+// public class Day9{
+    
+//     public static void main(String[] args){
+//         int age=5;
+//         try{
+//             if(age<18){
+//                 throw new ArithmeticException("Age must be at least 18");
+//             }
+//             System.out.println("You are eligible for the vote.");
+//         }catch(ArithmeticException e){
+//             System.out.println("Exception: "+e.getMessage());
+//         }
+//         }
+//     }
