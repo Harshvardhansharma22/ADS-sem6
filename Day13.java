d303af460bfa985e01873a8257d6330f134c583f
Harshvardhan <harshvardhan584624@gmail.com>
2026-03-11 12:29:22 +0530

    commit15

---
+++ Day13.java
@@ -0,0 +1,9 @@
+// classs Solution {
+//     public ListNode deleteDuplicates(ListNode head){
+//         ListNode current = head;
+//         while(current != null)
+//     }
+// }
+
+
+// 2) Remove Linked List Elements
